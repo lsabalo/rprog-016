65cb0d83d27d8a1ca39383ffd027c0168bf4ca76
Lucas Sabalo <lsabalo@gmail.com>
2014-12-09 13:39:49 -0700

    Initial version

---
+++ best.R
@@ -0,0 +1,13 @@
+best <- function(state, outcome) {
+    # If invalid state is given, then stop with message "invalid state"
+    
+    # If invalid outcome is given, then stop with message "invalid outcome"
+    
+    ## Read outcome data
+    
+    ## Check that state and outcome are valid
+    
+    ## Return hospital name in that state with lowest 30-day death
+    
+    ## rate
+}
